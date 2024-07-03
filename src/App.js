22488d0bcec626b1c3b48915884a888509f11129
Niconord59 <nicolascampagne@hotmail.fr>
2024-07-03 18:52:39 +0200

    Add Login component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ClassroomPlanner from './ClassroomPlanner';
-import Login from './Login';
+import Login from './components/ui/Login'; // Assurez-vous que le chemin est correct
 import './App.css';
 
 function App() {
