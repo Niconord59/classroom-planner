b7bc88126e60097d887b99f5cec269c13ff0de87
Niconord59 <nicolascampagne@hotmail.fr>
2024-07-03 19:19:45 +0200

    Remove Login component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import ClassroomPlanner from './ClassroomPlanner';
-import Login from './components/ui/Login'; // Assurez-vous que le chemin est correct
+import React from 'react';
+import ClassroomPlanner from './ClassroomPlanner.js';
 import './App.css';
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            path="/login"
-            element={<Login setAuthenticated={setAuthenticated} />}
-          />
-          <Route
-            path="/"
-            element={authenticated ? <ClassroomPlanner /> : <Navigate to="/login" />}
-          />
-        </Routes>
-      </Router>
+      <ClassroomPlanner />
     </div>
   );
 }
